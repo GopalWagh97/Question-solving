66f0244cdc7601a6aa201ccfde93e2993a1170a9
GopalWagh97 <2024bcs097@sggs.ac.in>
2025-06-12 15:28:21 +0530

    By DutchFlagAlgorithm

--- arraysort.java
+++ arraysort.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 public class arraysort
 {
-    public static void main()
+    public static void main(String[]args)
     {
         Scanner sc=new Scanner(System.in);
         int arr[]={0,1,2,0,1,2,1,2,0,0};
